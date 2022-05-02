fd8c08555fa7462906a93657818100a533d08118
Alvaro <alvarobiazon@hotmail.com>
2022-05-02 19:23:09 -0300

    ignore, requirements, settings

--- good_browser_games/settings.py
+++ good_browser_games/settings.py
@@ -81,9 +81,6 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
