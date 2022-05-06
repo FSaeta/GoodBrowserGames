5e7038ef540205b385ab52deef23640d640d9bcf
Alvaro <alvarobiazon@hotmail.com>
2022-05-06 02:04:09 -0300

    Implementando avaliações

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, avaliacoes, marcar_como_util
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('avaliacoes', avaliacoes, name='avaliacoes'),
+    path('marcar_como_util/<int:av_id>', marcar_como_util, name='marcar_como_util')
 ]
