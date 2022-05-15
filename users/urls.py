5851c7d037aead6aa0c66b25e4be33799c21312c
Alvaro <alvarobiazon@hotmail.com>
2022-05-15 18:59:49 -0300

    Relatórios implementados e funcionando

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateUserView
+from .views import CreateUserView, relatorios
 
 urlpatterns = [
-    path('registration', CreateUserView.as_view(), name='cadastro_usuario')
+    path('registration', CreateUserView.as_view(), name='cadastro_usuario'),
+    path('relatorios/', relatorios, name='relatorios'),
+    path('relatorios/start=<str:start>/stop=<str:stop>', relatorios, name='relatorios-filtrados')
 ]
--- users/views.py
+++ users/views.py
@@ -19,6 +19,11 @@ class CreateUserView(CreateView):
     template_name = 'registration/user_registration.html'
     success_url = '/auth/login'
 
+class LinhaRelatorio:
+    def __init__(self, pos, nome, valor):
+        self.pos = pos
+        self.nome = nome
+        self.valor = valor
 
 class Relatorio:
     def __init__(self, start=0, stop=0, class_obj=BrowserGame, top=5):
@@ -27,14 +32,23 @@ class Relatorio:
         self.class_obj = class_obj
         self.top = top
         self.objects = self.compute_objects()
+        self.objs_len = len(self.objects)
+        self.nome = 'Games Com Mais Avaliações'
+    
+    def get_objects_vals(self, objects):
+        values = [x for x in objects.order_by('-av_count')[:self.top].values('av_count', 'nome')]
+        [values[x].update({'pos': x+1}) for x in range(len(values))]
+        return values
 
     def compute_objects(self):
         objs_grouped = self.class_obj.objects.annotate(av_count=Count('avaliacao__id'))
 
         objs_grouped = self.filter_date_objs(objs_grouped)
+        values = self.get_objects_vals(objs_grouped)
 
-        values = [x for x in objs_grouped.order_by('-av_count')[:self.top].values('av_count', 'nome')]
-        [values[x].update({'pos': x+1}) for x in range(len(values))]
+        values = [LinhaRelatorio(v['pos'], v['nome'], v['av_count']) for v in values]
+        values = self.ajustar_linhas(values)
+
         return values
 
     def filter_date_objs(self, objs):
@@ -43,8 +57,23 @@ class Relatorio:
         if self.stop:
             objs.filter(avaliacao__create_date__lte=self.stop)
         return objs
+    
+    def ajustar_linhas(self, values):
+        while len(values) < 5:
+            values.append(LinhaRelatorio('---', '-----', '---'))
+        return values
+    
+    def value_label(self):
+        return 'Qtd. Avaliações'
+    
+    def object_label(self):
+        return 'Browser Game'
 
 class RelatorioCategorias(Relatorio):
+    def __init__(self, start=0, stop=0, class_obj=Categoria, top=3):
+        super().__init__(start, stop, class_obj, top)
+        self.nome = 'Categorias Mais Avaliadas'
+
     def compute_objects(self):
         objs_grouped = self.class_obj.objects.annotate(
             av_count=Count('browsergame__avaliacao__id')
@@ -54,12 +83,20 @@ class RelatorioCategorias(Relatorio):
         if self.stop:
             objs_grouped.filter(browsergame__avaliacao__create_date__lte=self.stop)
 
-        values = [x for x in objs_grouped.order_by('-av_count')[:self.top].values('av_count', 'nome')]
-        [values[x].update({'pos': x+1}) for x in range(len(values))]
+        values = self.get_objects_vals(objs_grouped)
+        values = [LinhaRelatorio(v['pos'], v['nome'], v['av_count']) for v in values]
+        values = self.ajustar_linhas(values)
 
         return values
 
+    def object_label(self):
+        return 'Categoria'
+
 class RelatorioUsers(Relatorio):
+    def __init__(self, start=0, stop=0, class_obj=User, top=5):
+        super().__init__(start, stop, class_obj, top)
+        self.nome = 'Usuários que mais Avaliaram'
+
     def compute_objects(self):
         objs_grouped = self.class_obj.objects.annotate(
             av_count=Count('avaliacao__id')
@@ -67,12 +104,21 @@ class RelatorioUsers(Relatorio):
         objs_grouped = self.filter_date_objs(objs_grouped)
 
         values = [x for x in objs_grouped.order_by('-av_count')[:self.top].values('av_count', 'username')]
-        [values[x].update({'pos': x+1}) for x in range(len(values))]
+        [values[x].update({'pos': x+1, 'nome': values[x]['username']}) for x in range(len(values))]
+
+        values = [LinhaRelatorio(v['pos'], v['nome'], v['av_count']) for v in values]
+        values = self.ajustar_linhas(values)
 
         return values
 
+    def object_label(self):
+        return 'Usuário'
 
 class RelatorioMediaGames(Relatorio):
+    def __init__(self, start=0, stop=0, class_obj=BrowserGame, top=5):
+        super().__init__(start, stop, class_obj, top)
+        self.nome = 'Games com Maiores Médias'
+
     def compute_objects(self):
         objs_grouped = self.class_obj.objects.annotate(
             av_media=Avg('avaliacao__rating'), 
@@ -84,8 +130,13 @@ class RelatorioMediaGames(Relatorio):
         values = [x for x in objs_grouped.order_by('-av_media')[:self.top].values('av_media', 'av_count', 'nome')]
         [values[x].update({'pos': x+1}) for x in range(len(values))]
 
+        values = [LinhaRelatorio(v['pos'], v['nome'], v['av_media']) for v in values]
+        values = self.ajustar_linhas(values)
+
         return values
 
+    def value_label(self):
+        return 'Nota Média Avaliações'
 
 
 def criar_relatorios(start=0, stop=0):
@@ -101,8 +152,27 @@ def criar_relatorios(start=0, stop=0):
 def relatorios(request, start=0, stop=0):
     user = request.user
 
+    str_stop, str_start = '', ''
+
+    if request.method == 'POST':
+        if 'refresh-period' in request.POST:
+            str_start = request.POST.get('start', 0)
+            str_stop = request.POST.get('stop', 0)
+
+            return redirect(f'/user/relatorios/start={str_start}/stop={str_stop}')
+
+    if start:
+        str_start = start
+        start = datetime.datetime.strptime(start, '%Y-%m-%d')
+    if stop:
+        str_stop = stop
+        stop = datetime.datetime.strptime(stop, '%Y-%m-%d')
+
     context = {
         'user': user,
-        'relatorios': criar_relatorios(start, stop)
+        'relatorios': criar_relatorios(start, stop),
+        'start': str_start,
+        'stop': str_stop,
+        'range1_5': range(0, 5)
     }
     return render(request, 'relatorios.html', context)
