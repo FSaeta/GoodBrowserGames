8ab39ebf404d0b2add5d30c6838efa349b7e9166
Alvaro <alvarobiazon@hotmail.com>
2022-05-15 23:49:16 -0300

    Implementa tela dos games e a busca por games

--- games/urls.py
+++ games/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index, avaliacoes, marcar_como_util
+from .views import index, games, avaliacoes, marcar_como_util
 
 urlpatterns = [
     path('', index, name='index'),
+    path('games-<int:page>', games, name='games'),
+    path('games-<int:page>/orderby=<str:orderby>/filter=<str:filter>', games, name='game_search'),
+    path('games-<int:page>/orderby=<str:orderby>/filter=<str:filter>/search=<str:search>', games, name='game_search'),
     path('avaliacoes', avaliacoes, name='avaliacoes'),
     path('marcar_como_util/<int:av_id>', marcar_como_util, name='marcar_como_util')
 ]
--- games/views.py
+++ games/views.py
@@ -23,6 +23,69 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+class SearchFiltersOption:
+    def __init__(self, opt, string):
+        self.option = opt
+        self.str = string
+
+def get_search_options(kind):
+    """<option value="-create_date" selected>Mais Recentes</option>
+       <option value="create_date">Mais Antigos</option>
+       <option value="nome">Ordem Alfabética</option>
+       <option value="recomendados">Recomendados</option>
+    """
+    if kind == 'order':
+        options = [
+            ('-create_date', 'Mais Recentes'),
+            ('create_date', 'Mais Antigos'),
+            ('nome', 'Ordem Alfabética'),
+            ('recomendados', 'Recomendados')
+        ]
+    else:
+        options = [
+            ('nome', 'Nome'),
+            ('categoria', 'Categoria')
+        ]
+    return [SearchFiltersOption(option[0], option[1]) for option in options]
+
+def games(request, page=1, orderby='-create_date', filter='nome', search=False):
+    user = request.user
+
+    if request.method == 'POST':
+        if 'search_submit' in request.POST:
+            order_by = request.POST.get('orderby', orderby)
+            filter_by = request.POST.get('filter', filter)
+            search_by = request.POST.get('search', '').strip()
+
+            redirect_page = f'/games-1/orderby={order_by}/filter={filter_by}'
+            if search_by:
+                redirect_page += f'/search={search_by}'
+
+            return redirect(redirect_page)
+
+    if orderby == 'recomendados':
+        games_to_show = user.get_games_recomendados(30, False)
+    else:
+        games_to_show = BrowserGame.objects.order_by(orderby)
+
+    if filter and search:
+        if filter == 'nome':
+            games_to_show = games_to_show.filter(nome__icontains=search)
+        else:
+            games_to_show = games_to_show.filter(categoria__nome__icontains=search)
+
+    games_to_show = games_to_show[30*(page-1):30*page]
+
+    context = {
+        'user': user,
+        'games': games_to_show,
+        'order_options': get_search_options('order'),
+        'filter_options': get_search_options('filter'),
+        'search_by': search,
+        'filter_by': filter,
+        'order_by': orderby
+    }
+    return render(request, 'games.html', context)
 
 def avaliacoes(request):
     user = request.user
